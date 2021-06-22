82ed70fbc3b2c90fdd83ab9b7cc8c0c34cfc8335
Mitchel <mitchel101350@gmail.com>
2021-06-22 20:55:08 +0800

    add realtime.py

---
+++ realtime.py
@@ -0,0 +1,33 @@
+import datetime as dt
+
+from twelvedata import TDClient
+
+
+class RealtimeData:
+    def __init__(self, symbol):
+        self.td = TDClient(apikey='Your API_KEY')
+        self.symbol = symbol
+
+    def get_data(self, start, end, interval):
+        data = self.td.time_series(
+            symbol=self.symbol,
+            interval=interval,
+            start_date=start,
+            end_date=end,
+            order='asc'
+        )
+
+        return data
+
+    def get_historical(self, start=dt.date.today()-dt.timedelta(days=365), end=dt.date.today()):
+        data = self.td.time_series(
+            symbol=self.symbol,
+            interval='1day',
+            start_date=start,
+            end_date=end,
+            outputsize=5000,
+            order='desc'
+        ).as_pandas()
+
+        return data
+
